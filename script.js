603a3ed1467781421ca6e0080d80d7c2b9cb7ac9
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-02-07 13:37:54 +0000

    variables containing input created and tested

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const addNote = (ev) => {
     //stopping the page from reloading when form is submitted
     ev.preventDefault();
-    console.log("test one")
+    
+    //take the information from the title, subject and note sections
+    let title = document.getElementById('note-title').value;
+    let subject = document.getElementById('subject').value;
+    let note = document.getElementById('text-input').value;
+    console.log(`this is the title: ${title}, this is the subject:${subject}, this is the note:${note}`)
 }
 
 document.addEventListener('DOMContentLoaded', () => {
